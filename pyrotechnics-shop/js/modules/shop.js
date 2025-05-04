3819a84d64c706f4258e1d13d23108c268818825
Jacob <6260999@edu.vaniercollege.qc.ca>
2025-05-04 18:55:43 -0400

    Added search function to the shop page >:D

--- pyrotechnics-shop/js/modules/shop.js
+++ pyrotechnics-shop/js/modules/shop.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', initApp);
 function initApp() {
     console.log("initializing the app...");
     displayDefaultShopItems();
+    const searchBar = document.getElementById("searchBar");
+    searchBar.addEventListener('search', displaySearchedShopItems);
 }
 
 function displayDefaultShopItems() {
@@ -39,4 +41,43 @@ function displayDefaultShopItems() {
     .catch(error => {
       console.error("Failed to load catalog.json:", error);
     });
+}
+
+function displaySearchedShopItems() {
+  const container = document.getElementById("card-container");
+  container.innerHTML = "";
+
+  fetch("../data/catalog.json")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json(); // Parses JSON
+  })
+  .then(data => {
+    data.products.forEach(product => {
+      if (product.ItemTitle.toLowerCase().includes(searchBar.value.toLowerCase())) {
+      const card = document.createElement("div");
+      card.className = "pagecard";
+
+      container.appendChild(card);
+
+      const cardimage = document.createElement("img");
+      cardimage.className = "card-img-top";
+      cardimage.src = product.ThumbnailImage;
+      cardimage.alt = product.Description;
+
+      card.appendChild(cardimage);
+
+      const cardtitle = document.createElement("figcaption");
+      cardtitle.className = "linkpage";
+      cardtitle.textContent = product.ItemTitle;
+
+      card.appendChild(cardtitle);
+      }
+    });
+  })
+  .catch(error => {
+    console.error("Failed to load catalog.json: ", error);
+  });
 }
