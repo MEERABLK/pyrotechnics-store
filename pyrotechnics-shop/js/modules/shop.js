0a9190946012771e82fded46b7db327771b79fdf
Jacob <6260999@edu.vaniercollege.qc.ca>
2025-05-06 03:32:25 -0400

    Displayed prices above shop items, combined all display js functions into one, added filtering in ascending and descending order of price, made it so that these filters are applied alongside the search feature!

--- pyrotechnics-shop/js/modules/shop.js
+++ pyrotechnics-shop/js/modules/shop.js
@@ -2,50 +2,17 @@ document.addEventListener('DOMContentLoaded', initApp);
 
 function initApp() {
     console.log("initializing the app...");
-    displayDefaultShopItems();
+    displayFilteredShopItems();
     const searchBar = document.getElementById("searchBar");
-    searchBar.addEventListener('search', displaySearchedShopItems);
+    searchBar.addEventListener('input', displayFilteredShopItems);
+    const filterList = document.getElementById("filter")
+    filterList.addEventListener('input', displayFilteredShopItems);
 }
 
-function displayDefaultShopItems() {
-    const container = document.getElementById("card-container");
-
-    fetch("../data/catalog.json")
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json(); // Parses JSON
-    })
-    .then(data => {
-      data.products.forEach(product => {
-        const card = document.createElement("div");
-        card.className = "pagecard";
-
-        container.appendChild(card);
-
-        const cardimage = document.createElement("img");
-        cardimage.className = "card-img-top";
-        cardimage.src = product.ThumbnailImage;
-        cardimage.alt = product.Description;
-
-        card.appendChild(cardimage);
-
-        const cardtitle = document.createElement("figcaption");
-        cardtitle.className = "linkpage";
-        cardtitle.textContent = product.ItemTitle;
-
-        card.appendChild(cardtitle);
-      });
-    })
-    .catch(error => {
-      console.error("Failed to load catalog.json:", error);
-    });
-}
-
-function displaySearchedShopItems() {
+function displayFilteredShopItems() {
   const container = document.getElementById("card-container");
   container.innerHTML = "";
+  const filterList = document.getElementById("filter")
 
   fetch("../data/catalog.json")
   .then(response => {
@@ -55,12 +22,24 @@ function displaySearchedShopItems() {
     return response.json(); // Parses JSON
   })
   .then(data => {
+    if (filterList.value == "AscendingPrice") {
+      data.products.sort((a, b) => a.UnitPrice - b.UnitPrice);
+    }
+    if (filterList.value == "DescendingPrice") {
+      data.products.sort((a, b) => b.UnitPrice - a.UnitPrice);
+    }
+    
     data.products.forEach(product => {
-      if (product.ItemTitle.toLowerCase().includes(searchBar.value.toLowerCase())) {
+      if (product.ItemTitle.toLowerCase().includes(searchBar.value.toLowerCase())) { // Applies the search ALONGSIDE the filters
       const card = document.createElement("div");
       card.className = "pagecard";
 
       container.appendChild(card);
+      
+      const cardprice = document.createElement("figcaption");
+      cardprice.className = "linkpage";
+      cardprice.textContent = product.UnitPrice + "$";
+      card.appendChild(cardprice);
 
       const cardimage = document.createElement("img");
       cardimage.className = "card-img-top";
@@ -78,6 +57,6 @@ function displaySearchedShopItems() {
     });
   })
   .catch(error => {
-    console.error("Failed to load catalog.json: ", error);
+    console.error("Failed to load catalog.json:", error);
   });
 }
